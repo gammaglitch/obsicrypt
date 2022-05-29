04be9804cd4c1f226573aeb1051e8725d770eca4
GammaGlitch <122680687+gammaglitch@users.noreply.github.com>
2022-05-29 22:41:32 +0200

    wip: add vite/ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,37 @@
-import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
-import path = require('path');
+import preact from '@preact/preset-vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [preact()],
-	build: {
-		outDir: './build',
-		sourcemap: 'hidden',
-	},
 	resolve: {
 		alias: {
 			react: 'preact/compat',
 			'react-dom/test-utils': 'preact/test-utils',
 			'react-dom': 'preact/compat',
 			'react/jsx-runtime': 'preact/jsx-runtime',
-			'@': path.resolve(__dirname, './src'),
-		},
+			'@': path.resolve(__dirname, './src')
+		}
 	},
+	build: {
+		outDir: './build'
+	}
 });
+// import path = require('path');
+
+// export default defineConfig({
+// 	plugins: [preact()],
+// 	build: {
+// 		outDir: './build',
+// 		sourcemap: 'hidden',
+// 	},
+// 	resolve: {
+// 		alias: {
+// 			react: 'preact/compat',
+// 			'react-dom/test-utils': 'preact/test-utils',
+// 			'react-dom': 'preact/compat',
+// 			'react/jsx-runtime': 'preact/jsx-runtime',
+// 			'@': path.resolve(__dirname, './src'),
+// 		},
+// 	},
+// });
